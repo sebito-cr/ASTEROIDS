6517a88ff71a88e0f5fbf6516b6e26b718c5bb5d
Joshua Allen <90309098+Joshdpa@users.noreply.github.com>
2022-08-24 12:48:04 +0000

    Created the spaceship

---
+++ script.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  ship = new Ship();
+}
+
+function draw() {
+ background(0);
+  ship.render();
+}
+
+function Ship(){
+  this.pos = createVector(width/2, height/2);
+
+  // this.pos.x = width/2
+  // this.pos.y = height/2
+  
+  this.r = 15;
+  this.render = function(){
+   translate(this.pos.x, this.pos.y);
+   triangle (-this.r, this.r, this.r, this.r, 0, -this.r)
+  }
+}
+
