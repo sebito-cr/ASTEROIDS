94ae8291668599fea6d7f6d60ed5a80d92c93b32
Joshua Allen <90309098+Joshdpa@users.noreply.github.com>
2022-08-25 08:41:22 +0000

    Ship rotates

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ function setup() {
 }
 
 function draw() {
- background(0);
+  background(0);
   ship.render();
+  ship.turn(0.1);
 }
 
-function Ship(){
-  this.pos = createVector(width/2, height/2);
+function Ship() {
+  this.pos = createVector(width / 2, height / 2);
 
   // this.pos.x = width/2
   // this.pos.y = height/2
-  
-  this.r = 15;
-  this.render = function(){
-   translate(this.pos.x, this.pos.y);
-   triangle (-this.r, this.r, this.r, this.r, 0, -this.r)
-  }
-}
 
+  this.r = 15;
+  this.heading = 0;
+
+  this.render = function() {
+    translate(this.pos.x, this.pos.y);
+    rotate(this.heading)
+    noFill();
+    stroke(255);
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
+  }
+
+  this.turn = function(angle) {
+    this.heading += angle;
+  }
+
+}
