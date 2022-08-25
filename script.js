b4cbb14629914ff3a73f154a28105b06f8b688d5
Joshua Allen <90309098+Joshdpa@users.noreply.github.com>
2022-08-25 18:16:32 +0000

    Beweging geprobeerd, iets mist

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function draw() {
   background(0);
   ship.render();
   ship.turn();
+  ship.update();
 }
 
 function keyReleased(){
@@ -20,18 +21,25 @@ function keyPressed() {
     ship.setRotation(0.1);
   } else if (keyCode == LEFT_ARROW){
     ship.setRotation(-0.1);
+  }else if (keycode == UP_ARROW){
   }
 }
 
 function Ship() {
   this.pos = createVector(width / 2, height / 2);
-
-  // this.pos.x = width/2
-  // this.pos.y = height/2
-
   this.r = 15;
   this.heading = 0;
   this.rotation = 0;
+  this.vel = createVector (0,0);
+
+  this.update  = function(){
+    this.pos.add(this.vel);
+  }
+
+  this.boost = function(){
+    var force = p5.Vector.fromAngle(this.heading);
+    this.vel.add(force);
+  }
 
   this.render = function() {
     translate(this.pos.x, this.pos.y);
