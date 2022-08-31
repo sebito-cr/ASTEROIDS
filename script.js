53b2d94621dd719b630e8a8dee2a0bbd286c0368
Joshua Allen <90309098+Joshdpa@users.noreply.github.com>
2022-08-31 11:41:23 +0000

    Het hoort te bewegen...

--- script.js
+++ script.js
@@ -21,7 +21,8 @@ function keyPressed() {
     ship.setRotation(0.1);
   } else if (keyCode == LEFT_ARROW){
     ship.setRotation(-0.1);
-  }else if (keycode == UP_ARROW){
+  } else if (keycode == UP_ARROW){
+    ship.boost();
   }
 }
 
@@ -43,7 +44,7 @@ function Ship() {
 
   this.render = function() {
     translate(this.pos.x, this.pos.y);
-    rotate(this.heading)
+    rotate(this.heading + PI / 2)
     noFill();
     stroke(255);
     triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
