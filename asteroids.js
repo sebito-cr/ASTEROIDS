3415258d3b5755043256de0b1d76c79298abf08f
Sebito Liu <74767444+sebito-cr@users.noreply.github.com>
2022-09-07 11:44:18 +0000

    Start Asteroids

---
+++ asteroids.js
@@ -0,0 +1,10 @@
+function Asteroid() {
+  this.pos = createVector(random(width), random(height))
+  this.r = 50;
+
+  this.render = function(){
+  
+  translate(this.pos.x, this.pos.y);
+  ellipse(0,0, this.r * 2);
+  }
+}
