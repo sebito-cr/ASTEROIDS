aa55cafc48f9e468aeaf53fd3ff83b585c17adbb
Sebito Liu <74767444+sebito-cr@users.noreply.github.com>
2022-09-14 10:42:28 +0000

    Made Asteroids more Jaggedy

--- asteroids.js
+++ asteroids.js
@@ -1,8 +1,12 @@
 class Asteroid{
   constructor() {
   this.pos = createVector(random(width), random(height))
-  this.r = 50;
+  this.r = random(15, 50);
   this.total = floor(random(5, 15));
+  this.offset = [];
+  for (var i=0; i < this.total; i++) {
+    this.offset[i] = random(-10, 10);
+  }
   }
   
   render() {
@@ -14,8 +18,9 @@ class Asteroid{
     beginShape();
     for (var i = 0; i < this.total; i++) {
       var angle = map(i, 0, this.total, 0, TWO_PI);
-      var x = this.r * cos(angle);
-      var y = this.r * sin(angle);
+      var r = this.r + this.offset[i];
+      var x = r * cos(angle);
+      var y = r * sin(angle);
       vertex(x, y);
     }
     endShape(CLOSE);
