0eaa63650d6b1e40b7b6f0f7710abdb41b795102
Sebito Liu <74767444+sebito-cr@users.noreply.github.com>
2022-09-15 08:25:46 +0000

    Asteroids Edges

--- asteroids.js
+++ asteroids.js
@@ -2,11 +2,11 @@ class Asteroid{
   constructor() {
   this.pos = createVector(random(width), random(height))
   this.vel = p5.Vector.random2D();
-  this.r = random(15, 50);
+  this.r = random(70, 100);
   this.total = floor(random(5, 15));
   this.offset = [];
   for (var i=0; i < this.total; i++) {
-    this.offset[i] = random(-10, 10);
+    this.offset[i] = random(-15, 15);
   }
   }
 
@@ -29,8 +29,20 @@ class Asteroid{
       vertex(x, y);
     }
     endShape(CLOSE);
-
     pop();
-
   }
+
+    edges(){
+  if (this.pos.x > width + this.r){
+    this.pos.x = -this.r;
+  } else if (this.pos.x < -this.r){
+    this.pos.x = width +this.r;
+  }
+   if (this.pos.y > height + this.r){
+    this.pos.y = -this.r;
+  } else if (this.pos.y < -this.r){
+    this.pos.y = height +this.r;
+   }
+}
+
 }
