a1564d80483976912a83e12b7fa34a7a4f84a70c
Joshua Allen <90309098+Joshdpa@users.noreply.github.com>
2022-09-21 12:19:09 +0000

    put class ship in own script

---
+++ ship.js
@@ -0,0 +1,60 @@
+class Ship{
+  constructor(){
+  this.pos = createVector(width / 2, height / 2);
+  this.r = 23;
+  this.heading = 4.70;
+  this.rotation = 0;
+  this.vel = createVector (0,0);
+  this.isBoosting = false;
+  }
+
+  boosting(b){
+    this.isBoosting = b;
+  }
+
+  update(){
+    if (this.isBoosting){
+      this.boost();
+    }
+    this.pos.add(this.vel);
+    this.vel.mult(0.99);
+  }
+
+  boost(){
+    var force = p5.Vector.fromAngle(this.heading);
+    force.mult(0.1);
+    this.vel.add(force);
+  }
+
+  render() {
+    push ();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.heading + PI / 2)
+    fill(0);
+    stroke(255);
+    triangle(-this.r, this.r, this.r, this.r, 0, -this.r)
+    pop();
+  }
+
+  edges(){
+  if (this.pos.x > width + this.r){
+    this.pos.x = -this.r;
+  } else if (this.pos.x < -this.r){
+    this.pos.x = width +this.r;
+  }
+   if (this.pos.y > height + this.r){
+    this.pos.y = -this.r;
+  } else if (this.pos.y < -this.r){
+    this.pos.y = height +this.r;
+   }
+}
+  
+  setRotation(a){
+  this.rotation = a;
+}
+  
+  turn() {
+    this.heading += this.rotation;
+  }
+
+}
