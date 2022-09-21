758e7beaa9d1561595e76cd2f8258bac8f172dbe
Joshua Allen <90309098+Joshdpa@users.noreply.github.com>
2022-09-21 12:12:15 +0000

    problem split fix

--- asteroids.js
+++ asteroids.js
@@ -1,23 +1,26 @@
-class Asteroid{
+class Asteroid {
   constructor(pos) {
-  if (pos) {
-    this.pos = pos.copy;
-  }else {
-    this.pos = createVector(random(width), random(height))
-  }
-  this.vel = p5.Vector.random2D();
-  this.r = random(30, 80);
-  this.total = floor(random(5, 15));
-  this.offset = [];
-  for (var i=0; i < this.total; i++) {
-    this.offset[i] = random(-15, 15);
-  }
+    
+    if (pos) {
+      this.pos = pos.copy();
+    } 
+      
+    else {
+      this.pos = createVector(random(width), random(height))
+    }
+    this.vel = p5.Vector.random2D();
+    this.r = random(30, 80);
+    this.total = floor(random(5, 15));
+    this.offset = [];
+    for (var i = 0; i < this.total; i++) {
+      this.offset[i] = random(-15, 15);
+    }
   }
 
-  update(){
-    this.pos.add(this.vel);
- }
-  
+  update() {    
+      this.pos.add(this.vel); 
+  }
+
   render() {
     push();
     stroke(255);
@@ -36,24 +39,24 @@ class Asteroid{
     pop();
   }
 
-  breakup(){
-   var newA = [];
-   newA[0] = new Asteroid(this.pos); 
-   newA[1] = new Asteroid(this.pos);
-   return newA;
+  breakup() {
+    var newA = [];
+    newA[0] = new Asteroid(this.pos);
+    newA[1] = new Asteroid(this.pos);
+    return newA;
   }
-  
- edges(){
-  if (this.pos.x > width + this.r){
-    this.pos.x = -this.r;
-  } else if (this.pos.x < -this.r){
-    this.pos.x = width +this.r;
+
+  edges() {
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    } else if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
+    }
+    if (this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+    } else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r;
+    }
   }
-   if (this.pos.y > height + this.r){
-    this.pos.y = -this.r;
-  } else if (this.pos.y < -this.r){
-    this.pos.y = height +this.r;
-   }
-}
 
 }
